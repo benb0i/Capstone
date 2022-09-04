7ce01dfc3438df19e9537f5fa9825f639c86e4dd
BuildTools <unconfigured@null.spigotmc.org>
2022-09-04 12:26:00 -0700

    Added zip code functionality

---
+++ submitzip.js
@@ -0,0 +1,13 @@
+document.addEventListener("keydown", (event) => {
+    var key = event.which || event.keyCode;
+    if (key == 13) {
+        var enterbutton = document.getElementById("enterbutton");
+        enterbutton.click();
+    }
+}, false);
+
+function validate() {
+    alert("You've entered a ZIP Code");
+    window.open("bruh.html", '_self');
+    return false;
+}
